be730c1ba82e0309a08d104003ae2d3a3491b783
taypark <interrupt@kakao.com>
2021-05-14 03:48:07 +0900

    Add station router for backend service

--- src/routers/station.router.js
+++ src/routers/station.router.js
@@ -1,27 +1,40 @@
 import { Router } from 'express';
 import Station from '../models/station.model.js'
+import { apiResponser } from '../util/apiResponser.js';
 
 const stationRouter = Router();
 
-stationRouter.get('/', async (req, res, next) => {
+stationRouter.get('/all', async (req, res, next) => {
   const stations = await Station.find();
 
-  res.json({ length: stations.length, data: stations})
+  res.json({ length: stations.length, data: stations })
 });
 
-stationRouter.get('/sp', async (req, res, next) => {
-  const station = await Station.find({ stId: "EV002259" })
+stationRouter.get('/:stationId', async (req, res, next) => {
+  const { stationId } = req.params;
 
-  res.json(station)
+  try {
+    const targetStations = await Station.find({ stId: stationId })
+
+    apiResponser({ req, res, data: targetStations, message: "A station infomation" })
+  } catch (e) {
+    next(e)
+  }
 })
 
-stationRouter.get('/ds', async (req, res, next) => {
-  const station = await Station.find({ 
-    lat: { $gte: "37.624066871535", $lte: "37.800000000000" },
-    lng: { $gte: "126.1121671726", $lte: "128.000000000000" }
-  })
+stationRouter.post('/nearby', async (req, res, next) => {
+  const { minLat, maxLat, minLng, maxLng } = req.body;
 
-  res.json(station)
+  try {
+    const targetStations = await Station.find({
+      lat: { $gte: minLat, $lte: maxLat },
+      lng: { $gte: minLng, $lte: maxLng }
+    })
+
+    apiResponser({ req, res, data: targetStations, message: "Adjacent stations" })
+  } catch (e) {
+    next(e);
+  }
 })
 
 export default stationRouter;
