99d20e167ffc735cc7eb54e994ae5e26ab25efaf
taypark <interrupt@kakao.com>
2021-05-14 09:06:45 +0900

    Add seat available function

--- src/util/dataUpdateRoutine.js
+++ src/util/dataUpdateRoutine.js
@@ -2,6 +2,8 @@ import { promisify } from 'util';
 import { getChargerInfo } from './getFromOpenApi.js';
 import Station from '../models/station.model.js'
 import { logger } from './logger.js';
+import redisClient from './redisClient.js';
+import { sendMail } from './mailer.js';
 
 export const sleepPromise = promisify(setTimeout);
 
@@ -18,7 +20,25 @@ export const dataUpdateRoutine = async (req, res, next) => {
     await Station.insertMany(bulkData)
     logger.info(`*** Ended data update routine`)
 
+    // Redis 비교부
+    const redisStationsIds = await redisClient.keys('*')
+    for (const stationId of redisStationsIds) {
+      const chargers = await Station.find({ stId: stationId })
+      for (const charger of chargers) {
+        if (charger.chgerStat === "2") {
+          const redisMailReceivers = await redisClient.lrange(stationId, 0, -1)
+          const stationInfo = {
+            name: charger.stNm,
+            address: charger.addr
+          }
 
+          for (const receiver of redisMailReceivers) {
+            sendMail({ type: "available", stationInfo: stationInfo, emailReceiver: receiver })
+          }
+        }
+      }
+    }
+    
     break;
   }
 }
